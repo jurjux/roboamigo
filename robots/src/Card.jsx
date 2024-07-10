95b4590ca5d6e25eab0028f9d41424674487e9b8
lorenzo montaño <lorenzo.montano@hotmail.com>
2024-07-10 16:27:32 -0600

    multiples robots 1

---
+++ robots/src/Card.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const Card = () => {
+    return (
+<div> 
+    <img src="https://robohash.org/test?200x200" alt="robot img" />
+    <div>
+        <h2>jane caca</h2>
+        <p>jurjux@gmail.com</p>
+    </div>
+</div>
+    );
+}
+
+export default Card;
--- robots/src/main.jsx
+++ robots/src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Hello from './Hello.jsx'  // Importa el componente Hello
 import './index.css'
+import Card from './Card'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Hello />  {/* Usa el componente Hello */}
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
   </React.StrictMode>,
 )
