957b198d2282524f0185d87de836ea873bcc5f30
lorenzo montaño <lorenzo.montano@hotmail.com>
2024-07-10 17:41:24 -0600

    prjimer video

--- robots/src/Card.jsx
+++ robots/src/Card.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 
-const Card = () => {
+const Card = ({name, email, id }) => {
     return (
-<div> 
-    <img src="https://robohash.org/test?200x200" alt="robot img" />
+<div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5"> 
+    <img src={`https://robohash.org/${id}?200x200`} alt="robot img"/>
     <div>
-        <h2>jane caca</h2>
-        <p>jurjux@gmail.com</p>
+        <h2>{name}</h2>
+        <p>{email}</p>
     </div>
 </div>
     );
--- robots/src/main.jsx
+++ robots/src/main.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Card from './Card'
+import 'tachyons'
+import {robots} from './robots'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
+    <Card id={robots [0].id} name={robots[0].name} email ={robots[0].email}/>
+    <Card id={robots [1].id} name={robots[1].name} email ={robots[1].email}/>
+    <Card id={robots [2].id} name={robots[2].name} email ={robots[2].email}/>
+    <Card id={robots [3].id} name={robots[3].name} email ={robots[3].email}/>
+    <Card id={robots [4].id} name={robots[4].name} email ={robots[4].email}/>
   </React.StrictMode>,
 )
