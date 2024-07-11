192988bf85c37cf8fe262b6120d3b2a7aa439f49
lorenzo montaño <lorenzo.montano@hotmail.com>
2024-07-10 18:53:13 -0600

    barra busqueda chatgpt

---
+++ robots/src/App.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import 'tachyons';
+import { robots } from './robots';
+import './index.css';
+
+const App = () => {
+    const [searchField, setSearchField] = useState('');
+
+    const onSearchChange = (event) => {
+        setSearchField(event.target.value);
+    };
+
+    const filteredRobots = robots.filter(robot =>
+        robot.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
+    return (
+        <div className="tc">
+            <h1 className="f1">RoboFriends</h1>
+            <SearchBox searchChange={onSearchChange} />
+            <CardList robots={filteredRobots} />
+        </div>
+    );
+};
+
+export default App;
