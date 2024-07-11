0d40dfb1bd43b3cba2ab595e280598846dd97dc6
lorenzo montaño <lorenzo.montano@hotmail.com>
2024-07-10 21:06:49 -0600

    agrgo api

--- robots/src/App.jsx
+++ robots/src/App.jsx
@@ -1,13 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import 'tachyons';
-import { robots } from './robots';
 import './index.css';
-import './App.css'
+import './App.css';
 
 const App = () => {
+    const [robots, setRobots] = useState([]);
     const [searchField, setSearchField] = useState('');
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => setRobots(users));
+    }, []);
 
     const onSearchChange = (event) => {
         setSearchField(event.target.value);
@@ -21,7 +27,7 @@ const App = () => {
         <div className="tc">
             <h1 className="f1">RoboFriends</h1>
             <SearchBox searchChange={onSearchChange} />
-            <CardList robots={filteredRobots} />
+            {robots.length === 0 ? <h1>Loading</h1> : <CardList robots={filteredRobots} />}
         </div>
     );
 };
